1b97ae1ff113b9238266f1347b60d6168af68be8
ChoiGuitar <choigihongs@gmail.com>
2024-05-29 08:53:43 +0900

    initial commit

---
+++ app/src/main/java/kbbbc/app/random_seat/MainActivity.kt
@@ -0,0 +1,11 @@
+package kbbbc.app.random_seat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
