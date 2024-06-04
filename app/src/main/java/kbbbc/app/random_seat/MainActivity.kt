8c5e4a6837f8c760ca8d2028f4eb690d09e71965
cgh <cgh@diavision.co.kr>
2024-06-04 15:24:20 +0900

    fix) 화면 전체화면으로 표시되도록 수정

--- app/src/main/java/kbbbc/app/random_seat/MainActivity.kt
+++ app/src/main/java/kbbbc/app/random_seat/MainActivity.kt
@@ -2,10 +2,21 @@ package kbbbc.app.random_seat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import kbbbc.app.random_seat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
